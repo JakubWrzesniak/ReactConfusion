e9c154b21d28ae6d24bc6015f4e1fe252386b4a8
Jakub Wrzesniak <jakub.wrzesniak@outlook.com>
2020-09-30 15:26:19 +0200

    Fix error messages HomePage bugs

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -25,19 +25,19 @@ function RenderCard({item, isLoading, errMess}){
         );}
 }
 
-function Home(props){
+const Home = (props) => {
     return(
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.dish} 
-                        isLoading={props.dishesLoading}
-                        errMess={props.dishesErrMess}/>
+                <RenderCard item={props.dish} 
+                    isLoading={props.dishesLoading}
+                    errMess={props.dishesErrMess}/>
                 </div>
                 <div className="col-12 col-md m-1">
                 <RenderCard item={props.promotion} 
-                            isLoading={props.promoLoading} 
-                            errMess={props.promoErrMess} />
+                    isLoading={props.promoLoading} 
+                    errMess={props.promoErrMess} />
                 </div>
                 
                 <div className="col-12 col-md m-1">
